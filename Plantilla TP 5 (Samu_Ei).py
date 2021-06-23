7052297e92cb70eaa10a90d6e768d7a258805916
Eimi Saiz <83459802+EimiSaiz@users.noreply.github.com>
2021-06-23 16:25:36 -0300

    Add files via upload

---
+++ Plantilla TP 5 (Samu_Ei).py
@@ -0,0 +1,13 @@
+########################################################
+# Samuel Sisto - @samusisto
+# Eimi Saiz - @EimiSaiz
+# TP 5 - Eje. x: Xxx
+# UNRN Andina - Introducción a Ingeniería en Computación
+########################################################
+
+def prueba():
+
+    pass
+
+if __name__ == "__main__":
+    prueba()
